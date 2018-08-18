4c9d89f4ca66c1609afc82f275d4f9aa8d0fdfcc
Wan <yuxi.wan@gmail.com>
2018-08-18 12:30:44 +0800

    update home component to allow zoom meeting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AppVersion } from '@ionic-native/app-version';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,7 +36,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AppVersion
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+import { Platform } from 'ionic-angular';
+import { AppVersion } from '@ionic-native/app-version';
+
+declare var ZoomPlugin: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  plugindate;
 
+  constructor(public navCtrl: NavController, platform: Platform, appVersion: AppVersion) {
+
+    platform.ready().then(() => {
+
+
+      // contact.find(['displayName']).then((c) => {
+      //   console.log("Printing contacts:", c)
+      //   this.contacts = c
+      // });
+
+      console.log(ZoomPlugin);
+
+      ZoomPlugin.getDate((data) => {
+        console.log("in getDate() callback," + data);
+        this.plugindate = data;
+      });
+
+
+    });
+  } // end of constructor
+
+
+  joinZoom() {
+    console.log("Join button clicked");
+
+    ZoomPlugin.test((data) => {
+      console.log("in test() callback," + data);
+    });
   }
-
 }
+
